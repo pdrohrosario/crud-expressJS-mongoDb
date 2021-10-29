0f1974b10c87a66e104d2832bbc78fd1fa2b0019
Pedro Rosario <pdro.h.rosario@gmail.com>
2021-10-28 22:04:27 -0300

    Codificação da senha do usuário, ajustes de código e redirecionamento de página.

--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
     email:{type:String, required:true},
@@ -18,9 +19,23 @@ class Login {
     async register(){
         this.validate();
 
-        if(this.errors.lenght > 0){
+        if(this.errors.length > 0){
             return;
         }
+
+        await this.userExists();
+
+        console.log(this.errors);
+        console.log(this.errors.lenght);
+
+        if(this.errors.length > 0){
+            return;
+        }
+    
+        //password
+        const salt = bcryptjs.genSaltSync();
+        this.body.password = bcryptjs.hashSync(this.body.password, salt);
+
         try{
             this.user = await LoginModel.create(this.body);
         }
@@ -55,6 +70,13 @@ class Login {
            password: this.body.password
         };
     }   
+
+    async userExists(){
+        const user = await LoginModel.findOne({ email: this.body.email});
+        if(user){
+            this.errors.push('Usuário já existe.');
+        }
+    }
 }
 
 module.exports = Login;
